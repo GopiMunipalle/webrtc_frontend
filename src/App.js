230d0a87946575937b72468c038a4ad726bf2be3
LeelaGopiKrishna <gopik@axzorait.com>
2025-01-20 13:02:12 +0530

    transports added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import "./App.css";
 import { useEffect } from "react";
 import Call from "./components/VideoCall.js";
 
-export const socket = io("https://71ad-183-82-1-224.ngrok-free.app", { autoConnect: false ,withCredentials:true});
+export const socket = io("https://71ad-183-82-1-224.ngrok-free.app", {
+  autoConnect: false,
+  withCredentials: true,
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
 function App() {
   useEffect(() => {
